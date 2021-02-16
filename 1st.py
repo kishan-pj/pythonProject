f7355f1f7b8e1e60aa7eeabd88881de6442f9440
Kishan Prasad Jaiswal <bhootk123@gmail.com>
2021-02-16 11:42:00 +0545

    Initial commit

---
+++ 1st.py
@@ -0,0 +1,60 @@
+print("hello")
+a = 10
+b = 10
+print(type(a))
+print(type('jack'))
+print(id(a))
+print(id(b))
+a = 10
+b = 20
+c = a + b
+print(c)
+# program to calculate the area of circle
+radius=7
+pie=22/7
+area= pie*radius**2
+print('the area of circle is',area)
+#program to calculate the area of rectangle
+length=5
+breadth=3
+area= length*breadth
+print('the area of rectangle',area)
+#program to calculate the area of circle
+radius=float(4)
+pi=float(3.14)
+area=pi*radius**2
+print('the area of circle is',area)
+#program to calculate the perimeter of rectangle
+length=4
+breadth=2
+perimeter=2*(length+breadth)
+print('the perimeter of rectangle is',perimeter)
+a=2
+b=-3
+a-=b
+print(a,b,)
+#program to calculate the volume of rectangle
+length=10
+breadth=10
+height=2
+volume=length*breadth*height
+print('the voume of rectangle',volume)
+a=('10','20','car','50')
+print(type(a))
+a=(1.2,3.5)
+print(type(a))
+#program to calculate the area of square
+length=2
+area=length**2
+print('the area of square',area)
+x=10
+y=10
+z=x+y
+print(z)
+
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,10 @@
+print("hello")
+a=10
+b=10
+c=20
+print(id(a))
+print(id(b))
+print(id(c))
+import keyword
+print(len(keyword.kwlist))
+print(keyword.kwlist)
